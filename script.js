e55de28c9392b47c82ab5094cd0afebef7158978
v1ct0r136 <valencialondonovictormanuel@gmail.com>
2025-01-23 21:13:42 -0500

    Se realizo la pagina casi en su totalidad. Css, Js, json y html

---
+++ script.js
@@ -0,0 +1,42 @@
+fetch('menu.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const menuContainer = document.getElementById('productos');
+        data.forEach(menuItem => {
+            const menuItemElement = document.createElement('div');
+            menuItemElement.classList.add('card');
+            menuItemElement.innerHTML = `
+                <div class="first-content">
+                    <img class="w-full p-2" src="${menuItem.image}" alt="${menuItem.name}">
+                </div>
+                <div class="second-content">
+                    <h2>${menuItem.nombre}</h2>
+                    <p>${menuItem.ingredientes}</p>
+                    <p class="menu-item__price">$${menuItem.precio}</p>
+                </div>
+            `;
+            menuContainer.appendChild(menuItemElement);
+        });
+
+        // Aquí empieza la parte interactiva del flip
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.addEventListener('click', function () {
+                card.classList.toggle('active');
+            });
+        });
+
+    })
+    .catch(error => {
+        console.error('Hubo un error:', error);
+    });
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+menu.classList.toggle("hidden");
+});
+
